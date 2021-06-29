3476a531bf9bf7befc6e244cfccf01d67d6a2676
fabricio <fabricio.jacob@outlook.com>
2021-06-28 22:40:24 -0300

    add

---
+++ src/main/java/com/github/fabriciolfj/apiecommerce/ApiEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.github.fabriciolfj.apiecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEcommerceApplication.class, args);
+	}
+
+}
