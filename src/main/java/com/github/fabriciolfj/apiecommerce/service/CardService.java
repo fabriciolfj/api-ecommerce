d6af9a67dab0b6b5c3cfd4c074262115dd83df01
fabricio <fabricio.jacob@outlook.com>
2021-07-02 22:10:05 -0300

    add

--- src/main/java/com/github/fabriciolfj/apiecommerce/service/CardService.java
+++ src/main/java/com/github/fabriciolfj/apiecommerce/service/CardService.java
@@ -7,8 +7,11 @@ import javax.validation.Valid;
 import java.util.Optional;
 
 public interface CardService {
-  void deleteCardById(String id);
-  Iterable<CardEntity> getAllCards();
-  Optional<CardEntity> getCardById(String id);
-  Optional<CardEntity> registerCard(@Valid final AddCardReq addCardReq);
+    void deleteCardById(String id);
+
+    Iterable<CardEntity> getAllCards();
+
+    Optional<CardEntity> getCardById(String id);
+
+    Optional<CardEntity> registerCard(@Valid final AddCardReq addCardReq);
 }
